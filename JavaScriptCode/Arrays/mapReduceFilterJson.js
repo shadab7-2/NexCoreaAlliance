7eb5fa0529530ed5d8e66124d1c63708711a60a7
Sahdab Ansari <sahdabansari972@gmail.com>
2025-10-22 22:17:09 +0530

    two sum added

--- JavaScriptCode/Arrays/mapReduceFilterJson.js
+++ JavaScriptCode/Arrays/mapReduceFilterJson.js
@@ -68,3 +68,20 @@ console.log(parsedUser.age); // "Shadab"
 localStorage.setItem("name", "Shadab");
 console.log(localStorage.getItem("name")); // Shadab
 localStorage.removeItem("name");
+function twoSum(nums, target) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+
+    map.set(nums[i], i);
+  }
+
+  return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
