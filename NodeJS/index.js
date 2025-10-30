d43ca5adbcfb81f736473b7a06fe78c49acdd0cf
Sahdab Ansari <sahdabansari972@gmail.com>
2025-10-30 15:18:11 +0530

    Node Js Tutorials

---
+++ NodeJS/index.js
@@ -0,0 +1,5 @@
+//  const data = require("./second");
+import { details, details2, details3 } from "./second.js"
+ console.log(details.name);
+ console.log(details2.city);
+ console.log(details3.language);
---
+++ NodeJS/second.js
@@ -0,0 +1,18 @@
+export let details = {
+  name: "Shadab Ansari",
+  age: 25,
+  occupation: "developer",
+};
+
+export let details2 = {
+  city: "Bombay",
+  country: "India",
+  hobby: "Photography",
+};
+
+export let details3 = {
+  language: "English",
+  experiance: 2,
+};
+
+// module.exports = {details, details2, details3}; 
