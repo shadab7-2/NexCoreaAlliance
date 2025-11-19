0f7a216aa86f81652a58dd43f563b366bc465c53
Sahdab Ansari <sahdabansari972@gmail.com>
2025-11-19 10:07:06 +0530

    counter app

--- Express/exp_mongo/index.js
+++ Express/exp_mongo/index.js
@@ -20,7 +20,23 @@ app.get("/", async (req, res) => {
 app.post('/add-student', (req, res)=>{
   Student.create(req.body);
   res.redirect('/');
-}) 
+});
+
+app.get('/delete-student/:id',async (req, res) =>{
+  await Student.findByIdAndDelete(req.params.id);
+  res.redirect('/')
+})
+
+app.get('/edit-student/:id', async (req, res) =>{
+  let student = await Student.findById(req.params.id)
+   Student.findByIdAndUpdate(req.params.id)
+    res.render('edit-student', {student})
+});
+
+app.post('/edit-student/:id', async(req, res) =>{
+  await Student.findByIdAndUpdate(req.params.id, req.body)
+  res.redirect('/')
+})
      
 app.listen(3000, () => {
   console.log("Server is running on the port : http://localhost:3000");
